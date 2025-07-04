65490b7fb8051c5553df1b8c111651f3dfa22066
Swanand Takalkar <takalkarswanand@gmail.com>
2025-07-04 11:20:00 +0530

    change current password,updating fullname ,email features added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -201,6 +201,44 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
 
 })
 
+const changeCurrentPassword = asyncHandler(async(req,res)=>{
+    const {oldPassword,newPassword} = req.body;
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect  = await user.isPasswordCorrect(oldPassword);
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Invalid old password")
+
+    }
+    user.password = newPassword;
+   await user.save()
+   return res.status(200).json(new ApiResponce (200,{},"Password changed succesfully"))
+     
+})
+
+const getCurrentUser = asyncHandler((req,res)=>{
+  return res.status(200).json(200,req.user,"current user fetched successfully")
+
+})
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    const {fullname,email} = req.body
+    if(!fullname || !email){
+        throw new ApiError(400,"All fields are required")
+    }
+    const user = User.findByIdAndUpdate(
+    req.user?._id,
+    {
+        $set:{
+            fullname:fullname,
+            email:email
+        }
+    },
+    {new:true}
+
+).select("-password")
+return res.status(200).json(new ApiResponce(200,user,"Account Details Updated"))
+})
+
+
  
  export {registerUser,loginUser,logoutUser,refreshAccessToken} 
  
